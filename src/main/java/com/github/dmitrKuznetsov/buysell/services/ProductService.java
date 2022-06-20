18fa56a18eb186521641d52a45d03eecc97f71f6
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2022-06-20 13:58:54 +0300

    Added ProductController, ProductService, Product model

---
+++ src/main/java/com/github/dmitrKuznetsov/buysell/services/ProductService.java
@@ -0,0 +1,37 @@
+package com.github.dmitrKuznetsov.buysell.services;
+
+import com.github.dmitrKuznetsov.buysell.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+    private long ID = 0;
+
+    {
+        products.add(new Product(++ID,"Playstation 5", "Simple description", 67000, "Krasnoyarsk", "Tomas"));
+        products.add(new Product(++ID,"Iphone 8", "Simple description", 24000, "Moscow", "David"));
+    }
+
+    public List<Product> listProducts() { return products;}
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public Product getProductById(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) return product;
+        }
+
+        return null;
+    }
+}
---
+++ src/test/java/com/github/dmitrKuznetsov/buysell/BuysellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dmitrKuznetsov.buysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuysellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
