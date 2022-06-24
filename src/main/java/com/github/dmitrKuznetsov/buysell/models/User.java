7a998d61ddcb299f5b53b01fbcd60277ecba6ef0
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2022-06-24 14:24:11 +0300

    Added user-info Added AdminService and AdminController Added user-edit

--- src/main/java/com/github/dmitrKuznetsov/buysell/models/User.java
+++ src/main/java/com/github/dmitrKuznetsov/buysell/models/User.java
@@ -55,6 +55,8 @@ public class User implements UserDetails {
 
     // security
 
+    public boolean isAdmin() { return roles.contains(Role.ROLE_ADMIN);}
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
